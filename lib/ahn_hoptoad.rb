4dff2c6e4ad97373af430d3477aacce428823bf4
Ben Klang <bklang@mojolingo.com>
2011-04-19 15:04:42 -0400

    Warn if there is an error posting the exception

--- lib/ahn_hoptoad.rb
+++ lib/ahn_hoptoad.rb
@@ -5,5 +5,12 @@ require 'toadhopper'
 notifier = Toadhopper.new(COMPONENTS.ahn_hoptoad[:apikey],
                           :notify_host => COMPONENTS.ahn_hoptoad[:host])
 Events.register_callback([:exception]) do |e|
-  notifier.post!(e)
+  result = notifier.post!(e)
+  if !result.errors.empty? || result.status != 200
+    ahn_log.warn "Error posting exception to Hoptoad!"
+    result.errors.each do |error|
+      ahn_log.warn "Hoptoad error: #{error}"
+    end
+    ahn_log.warn "Original exception message: #{e.message}"
+  end
 end
