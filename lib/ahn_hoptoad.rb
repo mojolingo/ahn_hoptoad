c17ad29f2396cbc9cf3f962afc7f8945767bd405
Ben Klang <bklang@mojolingo.com>
2011-04-20 16:05:13 -0400

    Better error checking

--- lib/ahn_hoptoad.rb
+++ lib/ahn_hoptoad.rb
@@ -6,9 +6,11 @@ notifier = Toadhopper.new(COMPONENTS.ahn_hoptoad[:apikey],
                           :notify_host => COMPONENTS.ahn_hoptoad[:host])
 Events.register_callback([:exception]) do |e|
   result = notifier.post!(e)
-  if !result.errors.empty? || result.status != 200
-    ahn_log.warn "Error posting exception to Hoptoad!"
-    result.errors.each do |error|
+
+  response = notifier.post!(e)
+  if !response.errors.empty? || !(200..299).include?(response.status.to_i)
+    ahn_log.warn "Error posting exception to Hoptoad! Response code #{response.status}"
+    response.errors.each do |error|
       ahn_log.warn "Hoptoad error: #{error}"
     end
     ahn_log.warn "Original exception message: #{e.message}"
