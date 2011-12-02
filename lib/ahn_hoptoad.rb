cf9879eecd6105d986a218147740f96a42a02a2d
Ben Klang <ben@alkaloid.net>
2011-12-02 15:18:28 -0500

    Fix silly duplicate notifications

--- lib/ahn_hoptoad.rb
+++ lib/ahn_hoptoad.rb
@@ -5,8 +5,6 @@ require 'toadhopper'
 notifier = Toadhopper.new(COMPONENTS.ahn_hoptoad[:apikey],
                           :notify_host => COMPONENTS.ahn_hoptoad[:host])
 Events.register_callback([:exception]) do |e|
-  result = notifier.post!(e)
-
   response = notifier.post!(e)
   if !response.errors.empty? || !(200..299).include?(response.status.to_i)
     ahn_log.warn "Error posting exception to Hoptoad! Response code #{response.status}"
